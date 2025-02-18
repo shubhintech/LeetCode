5cbc11da41f57e58e9d230075b86debc48961adf
Souvik Mondal <codewithsouvik1416@gmail.com>
2025-02-18 22:01:57 +0530

    Update 0016. 3Sum Closet.cpp

--- Problems/0016. 3Sum Closet.cpp
+++ Problems/0016. 3Sum Closet.cpp
@@ -11,7 +11,6 @@ public:
                 if (sum == target) {
                     return sum;
                 }
-                // Main thing
                 if (abs(sum - target) < abs(ans - target)) {
                     ans = sum;
                 }
